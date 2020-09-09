467e54c965b1992549cb31463d05b4a06b4c7a0d
sangjinKIM93 <larger611@gmail.com>
2020-09-09 21:34:06 +0900

    Initial Commit

---
+++ StanfordLecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordLecture
+//
+//  Created by 김상진 on 2020/09/09.
+//  Copyright © 2020 kipCalm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
